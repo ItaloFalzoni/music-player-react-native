7be6fe0b3088f3c0e138fb51e25802e17dab03c6
Ítalo Falzoni <italo.falzoni@gmail.com>
2021-02-23 13:28:48 -0300

    Revert "fixed Playlist screem"
    This reverts commit 3f14c3012113a6854951942415b0771d8901219b.
    
    fixed Playlist screen

--- src/components/Playlist/index.js
+++ src/components/Playlist/index.js
@@ -1,54 +1,24 @@
-import React, { useRef } from 'react'
-import { View, Image, Text, Dimensions } from 'react-native'
-import { TouchableOpacity } from 'react-native-gesture-handler'
-import RBSheet from 'react-native-raw-bottom-sheet'
+import React from 'react'
+import { View, Image, Text } from 'react-native'
 
 import playCircleIcon from '../../../assets/images/icons/play-circle.png'
-
-import PlayingNow from '../PlayingNow'
 
 import styles from './styles'
 
 export default function Playlist() {
-  const refRBSheet = useRef()
-
-  const heightSheet = Dimensions.get('window').height
   return (
-    <>
-      <View style={styles.content}>
-        <View style={styles.playlistProfile}>
-          <View style={styles.playlistIcon} />
-          <View style={styles.nameGroup}>
-            <Text style={styles.playlistName}>Playlist Name</Text>
-            <Text style={styles.artistName}>8 musics</Text>
-          </View>
+    <View style={styles.content}>
+      <View style={styles.playlistProfile}>
+        <View style={styles.playlistIcon} />
+        <View style={styles.nameGroup}>
+          <Text style={styles.playlistName}>Playlist Name</Text>
+          <Text style={styles.artistName}>8 musics</Text>
         </View>
-
-        <Text style={styles.playlistTime}>28:40</Text>
-
-        <TouchableOpacity onPress={() => refRBSheet.current.open()}>
-          <Image style={styles.actionIcon} source={playCircleIcon} />
-        </TouchableOpacity>
       </View>
 
-      <RBSheet
-        ref={refRBSheet}
-        closeOnDragDown={true}
-        closeOnPressMask={false}
-        height={heightSheet}
-        animationType={'slide'}
-        closeOnDragDown={true}
-        customStyles={{
-          wrapper: {
-            backgroundColor: "transparent"
-          },
-          draggableIcon: {
-            backgroundColor: "#000"
-          }
-        }}
-      >
-        <PlayingNow />
-      </RBSheet>
-    </>    
+      <Text style={styles.playlistTime}>28:40</Text>
+
+      <Image style={styles.actionIcon} source={playCircleIcon} />
+    </View>
   )
 }
