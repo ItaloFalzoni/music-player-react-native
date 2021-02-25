1e566ca437c5cab4c80f65de9aafe890d2229061
Ítalo Falzoni <italo.falzoni@gmail.com>
2021-02-25 13:43:32 -0300

    added modal on playlist screen

--- src/pages/PlaylistScreen/index.js
+++ src/pages/PlaylistScreen/index.js
@@ -1,6 +1,7 @@
-import React from 'react'
-import { Text, Image } from 'react-native'
+import React, { useState } from 'react'
+import { Text, Image, View, Pressable } from 'react-native'
 import { ScrollView, TouchableOpacity } from 'react-native-gesture-handler'
+import Modal from 'react-native-modal'
 
 import Playlist from '../../components/Playlist'
 import styles from './styles'
@@ -8,17 +9,111 @@ import styles from './styles'
 import plusCircleIcon from '../../../assets/images/icons/plus-circle.png'
 
 export default function PlaylistScreen() {
-  return (
-    <ScrollView style={styles.scrollView}>
-      <Playlist />
-      <Playlist />
+  playlistArray = [
+    {
+      id: 1,
+      title: 'Playlist'
+    },
+    {
+      id: 2,
+      title: 'Playlist'
+    },
+    {
+      id: 3,
+      title: 'Playlist'
+    },
+    {
+      id: 4,
+      title: 'Playlist'
+    },
+    {
+      id: 5,
+      title: 'Playlist'
+    },
+    {
+      id: 6,
+      title: 'Playlist'
+    },
+    {
+      id: 7,
+      title: 'Playlist'
+    },
+    {
+      id: 8,
+      title: 'Playlist'
+    },
+    {
+      id: 9,
+      title: 'Playlist'
+    },
+    {
+      id: 10,
+      title: 'Playlist'
+    },
+  ]
 
-      <TouchableOpacity
-        style={styles.newPlaylist}
-        onPress={() => {}}>
-        <Image style={styles.actionIcon} source={plusCircleIcon} />
-        <Text style={styles.newPlaylistText}>New Playlist</Text>
-      </TouchableOpacity>
-    </ScrollView>
+  const playlistRenderArray = playlistArray.map(playlist => (
+    <View style={styles.playlistArray} key={playlist.id}>
+      <View style={styles.playlistArrayCheckbox} />
+      <Text style={styles.modalText}>Playlist {playlist.id}</Text>
+    </View>
+  ))
+
+  const [isModalVisible, setModalVisible] = useState(false);
+  
+  function toggleModal() {
+    setModalVisible(!isModalVisible);
+  }
+
+  return (
+    <>
+      <ScrollView style={styles.scrollView}>
+        <Playlist />
+        <Playlist />
+
+        <TouchableOpacity
+          style={styles.newPlaylist}
+          onPress={toggleModal}>
+          <Image style={styles.actionIcon} source={plusCircleIcon} />
+          <Text style={styles.newPlaylistText}>New Playlist</Text>
+        </TouchableOpacity>
+      </ScrollView>
+
+      <Modal
+        isVisible={isModalVisible}
+        animationIn={'fadeIn'}
+        animationOut={'fadeOut'}
+        // hideModalContentWhileAnimating={true}
+      >
+        <View style={styles.modalContainer}>
+          <View style={styles.modalHeader}>
+            <Text style={styles.modalHeaderText}>Add to playlist</Text>
+          </View>
+          <ScrollView style={styles.playlistArrayContainer}>
+            {playlistRenderArray}
+          </ScrollView>
+
+          <View style={styles.buttonRow}>
+            <Pressable
+              style={[styles.buttonModal, styles.buttonCancelModal]}
+              onPress={toggleModal}
+            >
+              <Text style={styles.modalButtonText}>
+                Cancel
+              </Text>
+            </Pressable>
+
+            <Pressable
+              style={[styles.buttonModal, styles.buttonDoneModal]}
+              onPress={toggleModal}
+            >
+              <Text style={styles.modalButtonText}>
+                Done
+              </Text>
+            </Pressable>
+          </View>
+        </View>
+      </Modal>
+    </>
   )
 }
--- src/pages/PlaylistScreen/styles.js
+++ src/pages/PlaylistScreen/styles.js
@@ -1,8 +1,8 @@
-import { StyleSheet } from 'react-native'
+import { StyleSheet, Dimensions } from 'react-native'
 
 const styles = StyleSheet.create({
   scrollView: {
-    paddingTop: 15
+    paddingTop: 5
   },
   newPlaylist: {
     flex: 1,
@@ -23,6 +23,76 @@ const styles = StyleSheet.create({
   actionButton: {
     width: 36,
     height: 36
+  },
+  modalContainer: {
+    height: Dimensions.get('window').height / 5 * 3,
+    backgroundColor: '#FFF',
+    justifyContent: 'space-between',
+    borderRadius: 5,
+  },
+  modalHeader: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingVertical: 20,
+    borderBottomWidth: 1,
+    borderColor: '#E5E5E5',
+    marginHorizontal: 10,
+  },
+  modalHeaderText: {
+    fontFamily: 'Poppins_700Bold',
+    fontSize: 18,
+    lineHeight: 21,
+    color: '#171516',
+  },
+  modalText: {
+    fontFamily: 'Poppins_400Regular',
+    fontSize: 16,
+    lineHeight: 21,
+    color: '#171516',
+  },
+  buttonRow: {
+    flexDirection: 'row',
+  },
+  buttonModal: {
+    paddingVertical: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+    flex: 0.5,
+  },
+  buttonCancelModal: {
+    backgroundColor: 'red',
+    borderBottomLeftRadius: 5,
+    marginRight: 1,
+  },
+  buttonDoneModal: {
+    backgroundColor: 'green',
+    borderBottomRightRadius: 5,
+    marginLeft: 1
+  },
+  modalButtonText: {
+    fontFamily: 'Poppins_400Regular',
+    fontSize: 16,
+    lineHeight: 21,
+    color: '#fff',
+  },
+  playlistArrayContainer: {
+    display: 'flex',
+    flexDirection: 'column'
+  },
+  playlistArrayCheckbox: {
+    height: 20,
+    width: 20,
+    borderWidth: 1,
+    marginRight: 30,
+  },
+  playlistArray: {
+    borderBottomWidth: 1,
+    borderColor: '#E5E5E5',
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingVertical: 16,
+    paddingHorizontal: 30,
   },
 })
 export default styles
